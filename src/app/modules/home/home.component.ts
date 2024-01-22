06e1ece2f8b02857bba9b0b8d4aa4abbc5605b8d
Victor Kruger <victormatheusk@gmail.com>
2024-01-22 17:32:30 -0300

    Initial Commit

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmitLoginForm(): void {
+    console.log("Console Log");
+  }
+
+  onSubmitSignupForm(): void {
+    console.log("Console Log");
+  }
+}
